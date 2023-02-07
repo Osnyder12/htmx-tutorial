2b82f35fe7c188a782524f08355ce61854fd8e28
Osnyder12 <snyderowen12@gmail.com>
2023-02-07 15:43:00 -0500

    added homepage tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home_pages#htmx_homepage"
 end
